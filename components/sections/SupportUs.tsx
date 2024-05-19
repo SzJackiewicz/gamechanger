70585e18b97591b23743d50565065f0baba8540a
Szymon Jackiewicz <sz.jackiewicz@gmail.com>
2024-05-19 14:43:28 +0200

    zmiana buttonów stripe

--- components/sections/SupportUs.tsx
+++ components/sections/SupportUs.tsx
@@ -1,6 +1,6 @@
 'use server'
 
-import { BuyButtons } from '../slider/BuyButtons'
+import { BuyButton } from '../elements/BuyButton'
 
 export const SupportUs = () => {
   return (
@@ -10,8 +10,25 @@ export const SupportUs = () => {
           <div className='center-flex mt-50'>
             <h3 className='color-linear d-inline-block wow animate__animated animate__fadeInUp m-auto '>Wesprzyj nas</h3>
           </div>
-          <div className='row'>
-            <BuyButtons />
+          <div className='row center-flex-between'>
+            <BuyButton
+              slug='https://donate.stripe.com/3cs03M0RCfh9cX6dQQ'
+              title='Wsparcie jednorazowe'
+              price='25,00'
+              buttonTitle='Przekaż darowiznę'
+            />
+            <BuyButton
+              slug='https://buy.stripe.com/3cseYG9o82une1adQU'
+              title='Wsparcie cykliczne'
+              price='25,00'
+              buttonTitle='Przekaż darowiznę'
+              cycle
+            />
+            <BuyButton
+              slug='/wesprzyj-nas'
+              title='Pozostałe formy wsparcia'
+              buttonTitle='Wesprzyj nas'
+            />
           </div>
         </div>
       </div>
