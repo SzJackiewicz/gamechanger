e6294098fe001858551c01f439f189fec88e7c73
Szymon Jackiewicz <sz.jackiewicz@gmail.com>
2024-06-08 12:16:18 +0200

    Poprawki do lighthouse

--- pages/pomoc/index.tsx
+++ pages/pomoc/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Head from 'next/head'
 import Layout from '../../components/layout/Layout'
-import { HelpLines } from '@/utils/needHelpData'
+import { HelpLines } from '@/components/sections/needHelpData'
 import { CoverWithTestimonials } from '@/components/elements/CoverWithTestimonials'
 import { PartnersLogs } from '@/components/sections/PartnersLogs'
 
